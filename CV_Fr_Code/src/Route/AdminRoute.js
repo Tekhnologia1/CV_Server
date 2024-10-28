2cde8c6a6c341bc514e860aa9f5c3eb1a3809137
trevrushi7 <151149588+trevrushi7@users.noreply.github.com>
2024-10-28 18:11:44 +0530

    additions by Rushi

--- CV_Fr_Code/src/Route/AdminRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom"
-import { Auth } from "src/auth/AuthUser"
-
-export const AdminPrivateRoute = ({children}) => {
-    
-   return (Auth.getUserRole() === 'SchoolAdmin') ? children : <Navigate to="/login" />
-}
